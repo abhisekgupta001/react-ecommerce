973a10b05a46dc7d557dbe173550f29db876e3d0
Abhisek Gupta <guptaabhi6488@gmail.com>
2022-07-20 20:49:31 +0530

    feat: added homepage with sections

---
+++ src/constants/images.js
@@ -0,0 +1,7 @@
+import heroImg from "../assets/hero-image.png";
+import menImg from "../assets/men.jpg";
+import newImg from "../assets/new_arrival.jpg";
+import womenImg from "../assets/women.jpg";
+
+
+export default {heroImg,menImg,newImg,womenImg};
