7f791e7eba50e780333f8948272bef0751fb4448
Abhisek Gupta <guptaabhi6488@gmail.com>
2022-07-26 17:15:54 +0530

    refactor: implemented react router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Account from './pages/Account';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import RequiresAuth from './RequiresAuth';
-// import PrivateRoute from './PrivateRoute';
 
 function App(){
 
@@ -22,9 +21,6 @@ function App(){
         <Route path='/account' element={<RequiresAuth><Account/></RequiresAuth>}/>
         <Route path='/wishlist' element={<RequiresAuth><Wishlist/></RequiresAuth>}/>
         <Route path='/cart' element={<RequiresAuth><Cart/></RequiresAuth>}/>
-        {/* <PrivateRoute path="/account" element={<Account/>}/> */}
-        {/* <PrivateRoute path="/wishlist" element={<Wishlist/>}/> */}
-        {/* <PrivateRoute path="/cart" element={<Cart/>}/> */}
         <Route path='*' element={<NotFound/>}/>
       </Routes>                                                                                                                                                             																			                             
     </div>
